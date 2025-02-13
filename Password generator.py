f9d90e7133e5b78eae66b7c37da0d1b30757e02b
heyayush2010 <heyayush2010@gmail.com>
2025-02-13 15:01:33 +0000

    Add files via upload

---
+++ Password generator.py
@@ -0,0 +1,14 @@
+import random
+
+LETTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 
+           'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+SYMBOLS = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '{', '|', '}', '~']
+
+# Combine all the characters
+all_characters = LETTERS + NUMBERS + SYMBOLS
+
+# Generate a random password of length 8
+password = ''.join(random.choice(all_characters) for _ in range(8))
+
+print(password)
